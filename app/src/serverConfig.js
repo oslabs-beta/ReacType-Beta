9cdfaacc8e488489f921da69bf4793707b272885
noahFavelo <nnfavelo@gmail.com>
2025-01-14 20:26:41 -0500

    ok, before i move on with debug, sane test to see if it is thinking its dev mode still

--- app/src/serverConfig.js
+++ app/src/serverConfig.js
@@ -4,11 +4,11 @@ const serverConfig = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
     ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
-    : 'http://localhost:5656',
+    : 'http://localhost:9999',
   // : 'http://localhost:8080',
   API_BASE_URL2: isProduction
     ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
-    : 'http://localhost:8080'
+    : 'http://localhost:9999'
 };
 // module.exports = config;
 
