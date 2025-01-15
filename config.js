4a4595f6e70308ee6084515918b26da0c7082825
noahFavelo <nnfavelo@gmail.com>
2025-01-14 20:12:02 -0500

    ok, maybe config.js is actually fo rthe server and server confix is for clientgit add .??

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
+    ? 'http://localhost:5656'
     : 'http://localhost:5656',
   // Note, here is where you need to change if you are hosting on a differnt place.
   // : 'http://localhost:8080',
 
   // rn i am setting this to our url, but this could be made into an env variable. not that it changes much tho.
-  API_BASE_URL2: isProduction
-    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
+  API_BASE_URL2: isProduction //
+    ? 'http://localhost:5656'
     : 'http://localhost:8080'
 };
 module.exports = config;
