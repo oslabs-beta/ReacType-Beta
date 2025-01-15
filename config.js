21a0573bfd595f7ebc7a9e3c0c67871bd2b04956
noahFavelo <nnfavelo@gmail.com>
2025-01-14 19:34:11 -0500

    change api base url

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'http://localhost:5656'
+    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
     : 'http://localhost:5656',
   // Note, here is where you need to change if you are hosting on a differnt place.
   // : 'http://localhost:8080',
   API_BASE_URL2: isProduction
-    ? 'http://localhost:5656'
+    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
     : 'http://localhost:8080'
 };
 module.exports = config;
