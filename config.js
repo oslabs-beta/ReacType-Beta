f22bdbb02244888d643041d99de952e332f59a33
noahFavelo <nnfavelo@gmail.com>
2025-01-14 19:35:08 -0500

    comment config.js

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ const config = {
     : 'http://localhost:5656',
   // Note, here is where you need to change if you are hosting on a differnt place.
   // : 'http://localhost:8080',
+
+  // rn i am setting this to our url, but this could be made into an env variable. not that it changes much tho.
   API_BASE_URL2: isProduction
     ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
     : 'http://localhost:8080'
