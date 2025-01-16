8ae04095c64ec8a50c23050ecaf153282eb10ffd
noahFavelo <nnfavelo@gmail.com>
2025-01-16 15:24:39 -0500

    also add aws url to the config file

--- app/src/serverConfig.js
+++ app/src/serverConfig.js
@@ -3,11 +3,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 const serverConfig = {
   DEV_PORT: 5656,
   API_BASE_URL: isProduction
-    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
+    ? 'http://reactype.us-east-1.elasticbeanstalk.com'
     : 'http://localhost:9999',
   // : 'http://localhost:8080',
   API_BASE_URL2: isProduction
-    ? 'http://reactype.us-east-1.elasticbeanstalk.com/'
+    ? 'http://reactype.us-east-1.elasticbeanstalk.com'
     : 'http://localhost:9999'
 };
 // module.exports = config;
